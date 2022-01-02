56a73c505085740643d833eef2b0d580a22613aa
DFriend01 <devon.fri.2000@gmail.com>
2022-01-01 19:39:06 -0800

    Added descriptive error checking

--- src/RISCAssembler/InstructionParser.py
+++ src/RISCAssembler/InstructionParser.py
@@ -1,7 +1,7 @@
 import re
-from .InstructionTuples import Instruction
+from . import instructions
 from .SyntaxError import SyntaxError
-import pdb
+from . import errorcodes
 
 REGLEN = 2
 MINREG = 0
@@ -10,90 +10,6 @@ MAXNIBBLES = 4
 MEMORY_OPCODE = "101"
 INSTRUCTION_LENGTH_BINARY = 32
 INSTRUCTION_LENGTH_HEX = 4 #bytes
-
-INSTRUCTIONS_WITH_LITERALS = {
-    "MOV"     : Instruction("001", "00000", "000", 2),
-    "MOVE"    : Instruction("001", "00000", "001", 2),
-    "MOVNE"   : Instruction("001", "00000", "010", 2),
-    "MOVC"    : Instruction("001", "00000", "011", 2),
-    "MOVNC"   : Instruction("001", "00000", "100", 2),
-    "MOVLE"   : Instruction("001", "00000", "101", 2),
-    "MOVGE"   : Instruction("001", "00000", "110", 2),
-
-    "ADD"     : Instruction("010", "00000", "010", 2),
-    "ADDC"    : Instruction("010", "00001", "011", 2),
-    "AND"     : Instruction("010", "00010", "010", 2),
-    "CMP"     : Instruction("010", "00011", "000", 2),
-    "MVN"     : Instruction("010", "00100", "010", 2),
-    "NOT"     : Instruction("010", "00101", "010", 2),
-    "OR"      : Instruction("010", "00110", "010", 2),
-    "SUB"     : Instruction("010", "00111", "010", 2),
-    "SUBC"    : Instruction("010", "01000", "011", 2),
-    "TEST"    : Instruction("010", "01001", "000", 2),
-    "XOR"     : Instruction("010", "01010", "010", 2),
-
-    "JMP"     : Instruction("011", "00000", "000", 1),
-    "JE"      : Instruction("011", "00000", "001", 1),
-    "JNE"     : Instruction("011", "00000", "010", 1),
-    "JLE"     : Instruction("011", "00000", "101", 1),
-    "JGE"     : Instruction("011", "00000", "110", 1),
-
-    "FETCH"   : Instruction("101", "00001", "000", 2),
-    "STORE"   : Instruction("101", "00010", "000", 2)
-}
-
-INSTRUCTIONS_WITH_REGISTERS = {
-    "MOV"     : Instruction("001", "10000", "000", 2),
-    "MOVE"    : Instruction("001", "10000", "001", 2),
-    "MOVNE"   : Instruction("001", "10000", "010", 2),
-    "MOVC"    : Instruction("001", "10000", "011", 2),
-    "MOVNC"   : Instruction("001", "10000", "100", 2),
-    "MOVLE"   : Instruction("001", "10000", "101", 2),
-    "MOVGE"   : Instruction("001", "10000", "110", 2),
-
-    "ADD"     : Instruction("010", "10000", "010", 2),
-    "ADDC"    : Instruction("010", "10001", "011", 2),
-    "AND"     : Instruction("010", "10010", "010", 2),
-    "CMP"     : Instruction("010", "10011", "000", 2),
-    "MVN"     : Instruction("010", "10100", "010", 2),
-    "NOT"     : Instruction("010", "10101", "010", 2),
-    "OR"      : Instruction("010", "10110", "010", 2),
-    "SUB"     : Instruction("010", "10111", "010", 2),
-    "SUBC"    : Instruction("010", "11000", "011", 2),
-    "TEST"    : Instruction("010", "11001", "000", 2),
-    "XOR"     : Instruction("010", "11010", "010", 2),
-    "SL0"     : Instruction("010", "11011", "010", 1),
-    "SL1"     : Instruction("010", "11100", "010", 1),
-    "SR0"     : Instruction("010", "11101", "010", 1),
-    "SR1"     : Instruction("010", "11110", "010", 1),
-
-    "JMP"     : Instruction("011", "10000", "000", 1),
-    "JE"      : Instruction("011", "10000", "001", 1),
-    "JNE"     : Instruction("011", "10000", "010", 1),
-    "JLE"     : Instruction("011", "10000", "101", 1),
-    "JGE"     : Instruction("011", "10000", "110", 1),
-
-    "FETCH"   : Instruction("101", "10001", "000", 2),
-    "STORE"   : Instruction("101", "10010", "000", 2)
-}
-
-SPEICAL_INSTRUCTIONS = {
-    "HALT"    : Instruction("111", "00000", "000", 0),
-    "NOP"     : Instruction("000", "00000", "000", 0)
-}
-
-REGISTERS = {
-    "R0" : "0000",
-    "R1" : "0001",
-    "R2" : "0010",
-    "R3" : "0011",
-    "R4" : "0100",
-    "R5" : "0101",
-    "R6" : "0110",
-    "R7" : "0111",
-    "R8" : "1000",
-    "R9" : "1001"
-}
 
 def binbits(x, n):
     bits = bin(x).split('b')[1]
@@ -113,47 +29,47 @@ def hexbytes(x, n):
 class ErrorCheck:
 
     @staticmethod
-    def validInstructionName(name):
-        if name in INSTRUCTIONS_WITH_REGISTERS:
-            d = INSTRUCTIONS_WITH_LITERALS
-        elif name in SPEICAL_INSTRUCTIONS:
-            d = SPEICAL_INSTRUCTIONS
-        else:
-            raise SyntaxError()
+    def validInstructionName(name, linenumber, line):
+        if (not name in instructions.INSTRUCTIONS_WITH_REGISTERS) and (not name in instructions.SPEICAL_INSTRUCTIONS):
+            raise SyntaxError(linenumber, line, errorcodes.UNDEFINED_INSTRUCTION)
 
     @staticmethod
-    def validNumberOfArguments(actual_numargs, expected_numargs):
+    def validNumberOfArguments(actual_numargs, expected_numargs, linenumber, line):
         if actual_numargs != expected_numargs:
-            raise SyntaxError()
+            raise SyntaxError(linenumber, line, errorcodes.INCORRECT_NARGS)
 
     @staticmethod
-    def validHex(x):
+    def validHex(x, linenumber=0, line=""):
 
         hexsplit = x.lower().split('x')
         num = hexsplit[0] if len(hexsplit) < 2 else hexsplit[1]
+
         try:
             int(num, 16)
-            if (len(num) > MAXNIBBLES) or (x == "0x"):
+            if (x == "0x"):
                 raise ValueError
         except:
-            raise SyntaxError()
+            raise SyntaxError(linenumber, line, errorcodes.BAD_LITERAL)
 
+        if (len(num) > MAXNIBBLES):
+            raise SyntaxError(linenumber, line, errorcodes.BIG_LITERAL)
+        
     @staticmethod
-    def validRegister(register):
-        if(not register in REGISTERS):
-            raise SyntaxError()
+    def validRegister(register, linenumber, line):
+        if(not register in instructions.REGISTERS):
+            raise SyntaxError(linenumber, line, errorcodes.BAD_REGISTER)
 
 
 class InstructionParser:
 
     @staticmethod
-    def parse(line):
+    def parse(line, linenumber=0):
         instruction = InstructionParser.__get_instruction(line)
 
         if not instruction:
             return None
         else:
-            return InstructionParser.__encode_instruction(instruction)
+            return InstructionParser.__encode_instruction(instruction, line, linenumber)
 
     @staticmethod
     def __get_instruction(line):
@@ -170,22 +86,25 @@ class InstructionParser:
         return False
     
     @staticmethod
-    def __encode_instruction(instruction):
+    def __encode_instruction(instruction, line, linenumber):
         name = instruction[0]
-        ErrorCheck.validInstructionName(name)
+        ErrorCheck.validInstructionName(name, linenumber, line)
+
+        has_literal = False
 
         actual_numargs = len(instruction) - 1
-        if (actual_numargs == 0) and (name in SPEICAL_INSTRUCTIONS):
-            instruction_info = SPEICAL_INSTRUCTIONS[name]
-        elif (InstructionParser.__contains_literal(instruction[1:])) and (name in INSTRUCTIONS_WITH_LITERALS):
-            instruction_info = INSTRUCTIONS_WITH_LITERALS[name]
-        elif name in INSTRUCTIONS_WITH_REGISTERS:
-            instruction_info = INSTRUCTIONS_WITH_REGISTERS[name]
+        if (actual_numargs == 0) and (name in instructions.SPEICAL_INSTRUCTIONS):
+            instruction_info = instructions.SPEICAL_INSTRUCTIONS[name]
+        elif (InstructionParser.__contains_literal(instruction[1:])) and (name in instructions.INSTRUCTIONS_WITH_LITERALS):
+            instruction_info = instructions.INSTRUCTIONS_WITH_LITERALS[name]
+            has_literal = True
+        elif name in instructions.INSTRUCTIONS_WITH_REGISTERS:
+            instruction_info = instructions.INSTRUCTIONS_WITH_REGISTERS[name]
         else:
-            raise SyntaxError()
+            raise SyntaxError(linenumber, line, errorcodes.UNKNOWN_ERROR)
 
         expected_numargs = instruction_info.numargs
-        ErrorCheck.validNumberOfArguments(actual_numargs, expected_numargs)
+        ErrorCheck.validNumberOfArguments(actual_numargs, expected_numargs, linenumber, line)
 
         encoding = ""
 
@@ -195,42 +114,39 @@ class InstructionParser:
                      + instruction_info.cond + (20 * "0")
         elif actual_numargs == 1:
             # Single argument instruction that takes a literal
-            try:
-                ErrorCheck.validHex(instruction[1])
+            if has_literal:
+                ErrorCheck.validHex(instruction[1], linenumber, line)
                 encoding += instruction_info.opcode + instruction_info.op \
                          + instruction_info.cond + (4 * "0") \
                          + binbits(int(instruction[1], 16), 16)
 
             # Single argument instruction that takes a register
-            except:
-                ErrorCheck.validRegister(instruction[1])
+            else:
+                ErrorCheck.validRegister(instruction[1], linenumber, line)
                 encoding += instruction_info.opcode + instruction_info.op \
-                         + instruction_info.cond + REGISTERS[instruction[1]] \
+                         + instruction_info.cond + instructions.REGISTERS[instruction[1]] \
                          + (16 * "0")
             
         elif actual_numargs == 2:
-            ErrorCheck.validRegister(instruction[1])
+            ErrorCheck.validRegister(instruction[1], linenumber, line)
 
-            if instruction_info is INSTRUCTIONS_WITH_LITERALS[name]:
-                ErrorCheck.validHex(instruction[2])
+            if has_literal:
+                ErrorCheck.validHex(instruction[2], linenumber, line)
                 if instruction_info.opcode == MEMORY_OPCODE:
                     encoding += instruction_info.opcode + instruction_info.op \
-                             + instruction_info.cond + REGISTERS[instruction[1]] \
+                             + instruction_info.cond + instructions.REGISTERS[instruction[1]] \
                              + binbits(int(instruction[2][-2:], 16), 8) + (8 * "0")
                 else:
                     encoding += instruction_info.opcode + instruction_info.op \
-                             + instruction_info.cond + REGISTERS[instruction[1]] \
+                             + instruction_info.cond + instructions.REGISTERS[instruction[1]] \
                              + binbits(int(instruction[2], 16), 16)
             else:
-                ErrorCheck.validRegister(instruction[2])
+                ErrorCheck.validRegister(instruction[2], linenumber, line)
                 encoding += instruction_info.opcode + instruction_info.op \
-                             + instruction_info.cond + REGISTERS[instruction[1]] \
-                             + REGISTERS[instruction[2]] + (12 * "0")
+                             + instruction_info.cond + instructions.REGISTERS[instruction[1]] \
+                             + instructions.REGISTERS[instruction[2]] + (12 * "0")
         else:
-            raise SyntaxError()
+            raise SyntaxError(linenumber, line, errorcodes.UNKNOWN_ERROR)
 
         return hexbytes(int(encoding, 2), INSTRUCTION_LENGTH_HEX)
-
-    
-
 
--- src/RISCAssembler/SyntaxError.py
+++ src/RISCAssembler/SyntaxError.py
@@ -1,10 +1,53 @@
+import re
+from . import instructions
+from . import errorcodes
+
+def get_instruction(line):
+        return re.sub("#.*$", "", line).strip().upper().split()
+
 class SyntaxError(Exception):
 
-    def __init__(self, line="", errorcode=0, message="Syntax Error"):
-        self.errorcode = errorcode
-        self.line = line
-        self.message = message
+    def __init__(self, linenumber=0, line="", errorcode=errorcodes.UNKNOWN_ERROR):
+        self.message = SyntaxError.get_message(linenumber, line, errorcode)
         super().__init__(self.message)
 
     def __str__(self):
-        return f"{self.message}"
+        return self.message
+
+    @staticmethod
+    def get_message(linenumber, line, errorcode):
+        instruction = get_instruction(line)
+        name = instruction[0]
+        args = instruction[1:] if len(instruction) > 1 else []
+        message = f"SYNTAX ERROR on Line {linenumber}: " + line + "\n"
+
+        if (errorcode == errorcodes.UNDEFINED_INSTRUCTION):
+            message += "Undefined instruction " + name
+        elif (errorcode == errorcodes.INCORRECT_NARGS):
+            if name in instructions.INSTRUCTIONS_WITH_REGISTERS:
+                nargs = instructions.INSTRUCTIONS_WITH_REGISTERS[name].numargs
+            else:
+                nargs = instructions.SPEICAL_INSTRUCTIONS[name]
+            message += "Instruction " + name + " expected " + str(nargs) \
+                    +  " arguments, but " + str(len(args)) + " arguments were given"
+        elif (errorcode == errorcodes.BAD_REGISTER):
+            if (not instruction[1] in instructions.REGISTERS):
+                badreg = instruction[1]
+            else:
+                badreg = instruction[2]
+            message += badreg + " is expected to be a register R0 - R9"
+        elif (errorcode == errorcodes.BAD_LITERAL):
+            if (not instruction[1] in instructions.REGISTERS):
+                badliteral = instruction[1]
+            else:
+                badliteral = instruction[2]
+            message += badliteral + " is not a valid 2-byte hexadecimal value"
+        elif (errorcode == errorcodes.BIG_LITERAL):
+            if (not instruction[1] in instructions.REGISTERS):
+                bigliteral = instruction[1]
+            else:
+                bigliteral = instruction[2]
+            message += "Literal value should be 2 bytes, but " + bigliteral + " is too large"
+        else:
+            message += "An unknown error has occurred"
+        return message
