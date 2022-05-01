5e63611a2f5e165b353e61e1248255d5280d88a1
DFriend01 <devon.fri.2000@gmail.com>
2022-05-01 14:29:49 -0700

    Added arg parser

--- assemble.py
+++ assemble.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 import src.RISCAssembler.Assembler as asm
 
 DEBUG = False
@@ -7,54 +8,21 @@ DEBUG = False
 if not DEBUG:
     sys.tracebacklimit = 0
 
-HELP_CONTENT = "Usage: python3 assemble.py -i [INPUT PATH] -o [OUTPUT PATH] [OPTIONS]" + "\n" \
-    + "Compiles an assembly file formatted as a text file (.txt)" + "\n" \
-    + "Example: python3 assemble.py -i foo/input.txt -o bar/output.txt -s -b" + "\n" \
-    + "The input assembly file must exist and must be a text file" + "\n\n" \
-    + "    -i        Input path to assembly text file\n" \
-    + "    -o        Output path to write to text file\n" \
-    + "    -s        If output file already exists, do not overwrite it\n" \
-    + "    -b        Output binary instruction encodings. By default, instruction encodings are in hexadecimal\n" \
-    + "    --help    Display this message\n"
+# Define argument parser
+parser = argparse.ArgumentParser(description='RISC Assembler')
+parser.add_argument('-i', '--input-path', help='Input path to assembly text file')
+parser.add_argument('-o', '--output-path', help='Output path to write to text file')
+parser.add_argument('-s', '--safe-mode', help='If outfile already exists, do not overwrite it', 
+                    action='store_true', required=False)
+parser.add_argument('-b', '--binary', help='Output binary instruction encodings',
+                    action='store_true', required=False)
 
-def get_input_path():
-    try:
-        idx = sys.argv.index("-i")
-        if (idx == len(sys.argv) - 1):
-            raise LookupError
-        if (not os.path.exists(sys.argv[idx + 1])):
-            raise OSError
-        return sys.argv[idx + 1]
-    except ValueError:
-        print("The -i flag was not found")
-        raise
-    except LookupError:
-        print("Input path not found after -i flag")
-        raise
-    except OSError:
-        idx = sys.argv.index("-i")
-        path = sys.argv[idx + 1]
-        print(f"The input path {path} does not exist")
-        raise
+# Extract arguments
+args = parser.parse_args()
+infilename = args.input_path
+outfilename = args.output_path
+safe_mode = args.safe_mode
+output_binary = args.binary
 
-def get_output_path():
-    try:
-        idx = sys.argv.index("-o")
-        if (idx == len(sys.argv) - 1):
-            raise LookupError
-        return sys.argv[idx + 1]
-    except LookupError:
-        print("Output path not found after -o flag")
-        raise
-    except ValueError:
-        print("The -o flag was not found")
-        raise
-
-if "--help" in sys.argv:
-    print(HELP_CONTENT)
-else:
-    infilename = get_input_path()
-    outfilename = get_output_path()
-    safe_mode = "-s" in sys.argv
-    output_binary = "-b" in sys.argv
-    asm.Assembler.compile(infilename, outfilename, safe_mode, output_binary)
+# Compile the assembly code
+asm.Assembler.compile(infilename, outfilename, safe_mode, output_binary)
